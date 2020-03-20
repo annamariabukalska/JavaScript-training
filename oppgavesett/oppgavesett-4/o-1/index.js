987beba0819e36c676cd104ee9e7fca5c245f3de
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2020-03-20 13:51:01 +0100

    Added excercise 4-1

---
+++ oppgavesett/oppgavesett-4/o-1/index.js
@@ -0,0 +1,20 @@
+
+let movies = ["About time", "Legally blonde", "Eat, prey and love", "Aladdin", "Tom and Jerry"]
+
+console.log(movies[2], "Før oppdatering");
+
+movies.push("Side om side")  // Legger inn en film på slutten av arrayet
+//varer = [...varer, "Loff"] // Legger inn en film på slutten av arrayet
+
+
+movies.unshift("Jamses Bond") // Legger inn i starten av arrayet
+//nvarer = ["Sjokomelk", ...varer]  // Legger inn i starten av arrayet
+
+// varer = [] // Nuller ut arrayet mitt
+
+console.log(movies[2], "Etter oppdatering");
+
+// movies.splice(3,"Spartacus");
+//console.log(movies)
+
+
