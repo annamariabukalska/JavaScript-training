d9dec38a640456be6dc26d40420dc92ff6f21fcc
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2020-03-25 14:01:03 +0100

    Added exercise 3

---
+++ oppgavesett/oppgavesett-5/o-3/index.js
@@ -0,0 +1,39 @@
+const flute = document.querySelector("#flute");
+const trumpet = document.querySelector("#trumpet");
+const horn = document.querySelector("#horn");
+
+function playSound(name){
+
+    allStop();
+
+    if(name === "flute"){
+        flute.play();
+    }
+    if(name === "trumpet"){
+        trumpet.play();
+    }
+    if(name === "horn"){
+        horn.play();
+    }
+
+}
+
+function allStop(){
+    flute.pause();
+    trumpet.pause();
+    horn.pause();
+}
+
+/*
+function spillLydFluet() {
+    flute.play();
+}
+
+function spillLydTrumpet(){
+    trumpet.play();
+}
+
+function spillLydHorn(){
+    horn.play();
+}
+*/
