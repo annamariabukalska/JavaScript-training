e28429fb29996cab34d32c452a9eac7226d9f447
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2020-04-14 12:46:46 +0200

    Added images

--- oppgavesett/oppgavesett-7/o-1/index.js
+++ oppgavesett/oppgavesett-7/o-1/index.js
@@ -6,29 +6,48 @@ const flyknapper = document.querySelector("#flyknapper")
 const map = new mapboxgl.Map({
     container: 'kartet',
     style: 'mapbox://styles/mapbox/streets-v11',
-    zoom: 15,
-    center: [10.701317, 59.927157]
+    zoom: 10,
+    center: [10.730796, 59.912874]
 });
 
 const steder = [
     {
-        navn: "Sirkusteltet Frognerparken",
-        tekst: "Teater",
-        lng: 10.700877, 
-        lat: 59.928318,
+        navn: "Heidi",
+        tekst: "Bestevenner fra barneskolen",
+        lng: "10.760030",
+        lat: "59.946535",
+        bilde: "https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+
     },
     {
-        navn: "Vigelandsparken",
-        tekst: "Skulpturmuseum",
-        lng: 10.701371, 
-        lat: 59.927168,
+        navn: "Izabella",
+        tekst: "Bestevenner fra videregående skole",
+        lng: "10.754844",
+        lat: "59.928397",
+        bilde: "https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60"
+    },
+    {
+        navn: "Olga",
+        tekst: "Beste søstra i verden",
+        lng: "10.817885",
+        lat: "59.875949",
+        bilde: "https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
+
+    },
+    {
+        navn: "Molly",
+        tekst: "Den beste vennine i verden",
+        lng: "10.763360",
+        lat: "59.948853",
+        bilde: "https://images.unsplash.com/photo-1481214110143-ed630356e1bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60"
     }
 ]
 
 
 const addMarker = (lokal) => {
     const div = document.createElement("div")
-    div.className = "frognerparkenmarker"
+    div.className = "personer"
+    div.style = `background-image: url(${lokal.bilde})`
     const marker = new mapboxgl.Marker(div)
     const minPopup = new mapboxgl.Popup()
     minPopup.setHTML(`
